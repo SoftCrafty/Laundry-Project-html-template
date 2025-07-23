a8b63eff8eb7e240164294e06643a343898ee17b
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-07-23 09:24:33 +0600

    Update

--- js/custom.js
+++ js/custom.js
@@ -240,14 +240,44 @@
             dotsClass: 'docts-active-collect',
         });
         // Video Popup
-        $('.playBtn').magnificPopup({
-            type: 'iframe',
-            disableOn: 700,
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-            fixedContentPos: false
-        });
+        // $('.playBtn').magnificPopup({
+        //     type: 'iframe',
+        //     disableOn: 700,
+        //     mainClass: 'mfp-fade',
+        //     removalDelay: 160,
+        //     preloader: false,
+        //     fixedContentPos: false
+        // });
+
+        $(".playBtn").magnificPopup({
+            type : "iframe",
+            iframe: {
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '</div>', 
+
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
+                        id: 'v=', 
+                        src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+                    },
+                    vimeo: {
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
+                    },
+                    gmaps: {
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
+                    }
+
+                },
+
+                srcAction: 'iframe_src',
+              }
+        })
           
     });
         $(window).on("load", function () {
